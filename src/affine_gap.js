546dfed95df405d02ac4fcffeccd8377181edd04
Tianyi Shi <ShiTianyi2001@outlook.com>
2020-08-31 17:25:40 +0800

    correct algorithm

--- src/affine_gap.js
+++ src/affine_gap.js
@@ -1,16 +1,16 @@
 import { BLOSUM45 } from "./matrices/matrices.js";
-import { UP, LEFT, DIAG } from "./_params.js";
 import { compute_max_score_and_direction } from "./utils.js";
 import { Aligner } from "./nw.js";
+
+const UP_X = 1;
+const UP_U = 2;
+const LEFT_X = 3;
+const LEFT_L = 4;
+//const DIAG = 5;
 
 export class AffineGapAligner extends Aligner {
   constructor(x, y, match_fn, gap_open, gap_extend) {
     super(x, y, match_fn);
-    // this.x = x;
-    // this.y = y;
-    // this.n = x.length + 1;
-    // this.m = y.length + 1;
-    // this.match_fn = match_fn;
     this.gap_open = gap_open;
     this.gap_extend = gap_extend;
   }
@@ -22,51 +22,88 @@ export class AffineGapAligner extends Aligner {
     return this.report();
   }
   global_() {
-    this.init_matrices(true);
+    this.init_matrices_(true);
     this.fill_matrices_();
     this.score = this.S[this.x.length][this.y.length];
-    this.traceback_global();
+    this.traceback_global_();
     return this.report();
+  }
+  traceback_global_() {
+    this.traceback_([this.n - 1, this.m - 1]);
   }
   init_matrices(x) {
     this.D = Array.from({ length: this.n }, () => Array(this.m).fill(0));
     this.I = Array.from({ length: this.n }, () => Array(this.m).fill(0));
     this.S = Array.from({ length: this.n }, () => Array(this.m).fill(0));
     this.T = Array.from({ length: this.n }, () => Array(this.m).fill(0));
+    this.TD = Array.from({ length: this.n }, () => Array(this.m).fill(0));
+    this.TI = Array.from({ length: this.n }, () => Array(this.m).fill(0));
     for (let i = 1; i < this.n; i++) {
       //this.D[i][0] = this.gap_open + i * this.gap_extend;
       this.S[i][0] = this.gap_open + i * this.gap_extend;
       this.I[i][0] = -Infinity; // practically, no greater than this.S[i][0] + gap_open is OK
-      this.T[i][0] = UP;
+      this.T[i][0] = UP_X;
     }
     for (let j = 1; j < this.m; j++) {
       //this.I[0][j] = this.gap_open + j * this.gap_extend;
       this.S[0][j] = this.gap_open + j * this.gap_extend;
       this.D[0][j] = -Infinity;
-      this.T[0][j] = LEFT;
+      this.T[0][j] = LEFT_X;
     }
   }
   fill_matrices() {
     for (let i = 1; i < this.n; i++) {
       for (let j = 1; j < this.m; j++) {
-        [this.S[i][j], this.T[i][j]] = compute_max_score_and_direction(
-          (this.D[i][j] = Math.max(this.S[i - 1][j] + this.gap_open + this.gap_extend, this.D[i - 1][j] + this.gap_extend)), // delete (up)
-          (this.I[i][j] = Math.max(this.S[i][j - 1] + this.gap_open + this.gap_extend, this.I[i][j - 1] + this.gap_extend)),
-          this.S[i - 1][j - 1] + this.match_fn(this.x[i - 1], this.y[j - 1]) // match (diag)
-        ); //insert (left)
+        let up = this.S[i - 1][j] + this.gap_open + this.gap_extend;
+        let up_u = this.D[i - 1][j] + this.gap_extend;
+        let up_max = Math.max(up, up_u);
+        this.D[i][j] = up_max;
+        this.TD[i][j] = up_max === up ? 1 : 2;
+
+        let left = this.S[i][j - 1] + this.gap_open + this.gap_extend;
+        let left_l = this.I[i][j - 1] + this.gap_extend;
+        let left_max = Math.max(left, left_l);
+        this.I[i][j] = left_max;
+        this.TI[i][j] = left_max === left ? 3 : 4;
+
+        let diag = this.S[i - 1][j - 1] + this.match_fn(this.x[i - 1], this.y[j - 1]);
+
+        [this.S[i][j], this.T[i][j]] = compute_max_score_and_direction(up, up_u, left, left_l, diag);
       }
     }
   }
-  fill_matrices_() {
-    for (let i = 1; i < this.n; i++) {
-      for (let j = 1; j < this.m; j++) {
-        let up = this.S[i - 1][j] + this.gap_extend;
-        this.T[i - 1][j] !== UP && (up = up + this.gap_open);
-        let left = this.S[i][j - 1] + this.gap_extend;
-        this.T[i][j - 1] !== LEFT && (left = left + this.gap_open);
-        let diag = this.S[i - 1][j - 1] + this.match_fn(this.x[i - 1], this.y[j - 1]);
-        [this.S[i][j], this.T[i][j]] = compute_max_score_and_direction(up, left, diag);
-      }
+  traceback(coords) {
+    let res = [];
+    let [i, j] = coords;
+    let m = this.T;
+
+    while (true) {
+      //onsole.log(m, i, j);
+      let direction = m[i][j];
+      if (direction === UP_X) {
+        res.push(1);
+        i--;
+        m = this.T;
+      } else if (direction === UP_U) {
+        res.push(1);
+        i--;
+        m = this.TD;
+      } else if (direction === LEFT_X) {
+        res.push(2);
+        j--;
+        m = this.T;
+      } else if (direction === LEFT_L) {
+        res.push(2);
+        j--;
+        m = this.TI;
+      } else if (direction === 5) {
+        res.push(3);
+        i--;
+        j--;
+        m = this.T;
+      } else break;
     }
+    this.coords = [i, j];
+    this.alignment = res.reverse();
   }
 }
